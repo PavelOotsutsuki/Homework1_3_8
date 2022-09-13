227e5ed244781ed076d60ba24cce9f6679d9f4ba
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-09-13 22:22:59 +0300

    Добавьте файлы проекта.

---
+++ Homework1_3_8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Console.Write("Введите сдвиг влево: ");
+            int shiftLeft = Convert.ToInt32(Console.ReadLine());
+            shiftLeft = shiftLeft % array.Length;
+            int [] tempForChange = new int[shiftLeft];
+
+            for (int arrayIndex = 0; arrayIndex < shiftLeft; arrayIndex++)
+            {
+                tempForChange[arrayIndex] = array[arrayIndex];
+            }
+
+            for (int arrayIndex= 0; arrayIndex<array.Length- shiftLeft; arrayIndex++)
+            {
+                array[arrayIndex] = array[arrayIndex+ shiftLeft];
+            }
+
+            for (int arrayIndex = array.Length - shiftLeft; arrayIndex < array.Length; arrayIndex++)
+            {
+                array[arrayIndex] = tempForChange[arrayIndex - (array.Length - shiftLeft)];
+            }
+
+            for (int arrayIndex = 0; arrayIndex < array.Length; arrayIndex++)
+            {
+                Console.Write(array[arrayIndex] + " ");
+            }
+        }
+    }
+}
